79d2ee6be78090ddf9d522d47476514578d317cf
dennis-adrian <dennisguzmanbo@gmail.com>
2022-07-06 22:54:36 -0400

    Add README and sonarqube container configuration

---
+++ README.md
@@ -0,0 +1,5 @@
+# Important
+Script must be run with sudo permissions so that the commands needed to configure the machine for the sonarqube container can run
+```sh
+sudo bash homework1.sh
+```
--- homework1.sh
+++ homework1.sh
@@ -4,11 +4,30 @@ docker network create homework &&
 # creating postgres container
 docker run -d \
 --name postgres \
+-e POSTGRES_USER=root \
 -e POSTGRES_PASSWORD=root \
--e POSTGRES_USER=admin \
+-e PGDATA=/var/lib/postgresql/data/pgdata \
 -v postgres_data:/var/lib/postgresql/data \
 --network homework \
 postgres &&
+
+# creating sonarqube container
+sysctl -w vm.max_map_count=524288 &&
+sysctl -w fs.file-max=131072 &&
+ulimit -n 131072 &&
+ulimit -u 8192
+
+docker run -d \
+--name sonarqube \
+-p 9000:9000 \
+-e SONAR_JDBC_URL=jdbc:postgresql://postgres/postgres \
+-e SONAR_JDBC_USERNAME=root \
+-e SONAR_JDBC_PASSWORD=root \
+-v sonarqube_data:/opt/sonarqube/data \
+-v sonarqube_logs:/opt/sonarqube/logs \
+-v sonarqube_extensions:/opt/sonarqube/extensions \
+--network homework \
+sonarqube:8.9-community
 
 # creating jenkins container
 docker run -d \
